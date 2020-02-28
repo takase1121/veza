343ea7e27a6d1152b88af25d6176d197c8d9c7f9
takase1121 <idc>
2020-02-28 14:28:49 +0800

    remove binarytf completely yay

--- src/lib/Structures/MessageError.ts
+++ src/lib/Structures/MessageError.ts
@@ -1,31 +1,3 @@
-import { DeserializerError, DeserializerReason } from 'binarytf/dist/lib/errors/DeserializerError';
-
-/**
- * The MessageError class for deserializer errors
- * @since 0.7.0
- * @extends Error
- */
-export class MessageError extends Error {
-
-	/**
-	 * The kind of error from BinaryTF's error.
-	 * @since 0.7.0
-	 */
-	public kind: DeserializerReason;
-
-	/**
-	 * Constructs a MessageError instance.
-	 * @since 0.7.0
-	 * @param prefix The prefix indicating more information about the error.
-	 * @param error The DeserializerError instance to wrap.
-	 */
-	public constructor(prefix: string, error: DeserializerError) {
-		super(`${prefix}: ${error.message} [${error.kind}]`);
-		this.kind = error.kind;
-	}
-
-}
-
 /**
  * Creates an error.
  * @since 0.7.0
@@ -35,8 +7,6 @@ export class MessageError extends Error {
  * @private
  */
 export function makeError(prefix: string, error: Error) {
-	/* istanbul ignore else: Safe guard for edge cases. */
-	if (error instanceof DeserializerError) return new MessageError(prefix, error);
 	/* istanbul ignore next: Safe guard for edge cases. */
 	return new Error(`${prefix}: ${error.message}`);
 }
